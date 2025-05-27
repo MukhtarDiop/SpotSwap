5cfcc8f0d96c27c55af6e08857025a2479632085
Naika Estriplet <naika.c.3113@gmail.com>
2025-05-27 16:58:15 -0400

    resolved issues with sign up and log in, removed extra folders (booking view, booking controller), changed language of sign up button

--- app/models/booking.rb
+++
@@ -1,9 +0,0 @@
-class Booking < ApplicationRecord
-  belongs_to :user
-  belongs_to :spot
-
-  validates :status, presence: true
-  validates :start_date, presence: true
-  validates :end_date, presence: true
-  
-end
--- config/routes.rb
+++ config/routes.rb
@@ -18,12 +18,6 @@ Rails.application.routes.draw do
     end
   end
 
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
   # Defines the root path route ("/")
   # root "posts#index"
 
@@ -38,5 +32,5 @@ Rails.application.routes.draw do
   post "spots", to: "spots#create"
 
   # Read One
-  get "spots/:id", to: "spots#show", as: :spot
+  get "spots/:id", to: "spots#show"
 end
