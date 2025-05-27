b2a67a4c6a12ae0c18653bab19aba14955785510
33919848+MohdSafwanAhmad@users.noreply.github.com <safwanahmad006@gmail.com>
2025-05-27 11:27:56 -0400

    fix

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,14 +2,14 @@ class Spot < ApplicationRecord
   belongs_to :user
   CATEGORIES = ['storage', 'parking_space', 'garage']
 
-  validates :description, presence: :true
-  validates :lat, presence: :true
+  validates :description, presence: true
+  validates :lat, presence: true
   validates :lat, numericality: true
-  validates :long, presence: :true
+  validates :long, presence: true
   validates :long, numericality: true
-  validates :length, presence: :true
+  validates :length, presence: true
   validates :length, numericality: true
-  validates :width, presence: :true
+  validates :width, presence: true
   validates :width, numericality: true
 
   validates :category, inclusion: { in: CATEGORIES }
