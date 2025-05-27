2b60b27107fb5bb839ba5b50feb968390f7396a3
Naika Estriplet <naika.c.3113@gmail.com>
2025-05-27 12:23:17 -0400

    fixed errors in the validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "cleaning the database 🧼"
+Spot.delete_all
+User.delete_all
+
+puts "creating a user for the db"
+User.create(first_name: "Jon", last_name: "Smith", email: "test@testemail.com" )
+puts "finished creating the user"
+
+
+puts "creating a spot for the db"
