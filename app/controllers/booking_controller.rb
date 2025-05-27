ee72df29af6fb1bbd823d75c998c146de36efee7
Davy Lai Su Yeng <dvlsy@hotmail.com>
2025-05-27 13:10:49 -0400

    Add status action, route and view for bookings

---
+++ app/controllers/booking_controller.rb
@@ -0,0 +1,27 @@
+class BookingController < ApplicationController
+  before_action :authenticate_user!,:set_booking, only: %i[show edit update destroy]
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  def status
+    @user = current_user
+    @bookings = @user.bookings.find(params[:id])
+  end
+end
