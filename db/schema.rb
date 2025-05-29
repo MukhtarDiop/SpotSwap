31acb0bd5c4b4979a3d5081a314598c187662994
Naika Estriplet <naika.c.3113@gmail.com>
2025-05-29 17:34:34 -0400

    comitting changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_29_163608) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_28_184136) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -77,7 +77,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_29_163608) do
     t.string "address"
     t.float "latitude"
     t.float "longitude"
-    t.string "title"
     t.index ["user_id"], name: "index_spots_on_user_id"
   end
 
